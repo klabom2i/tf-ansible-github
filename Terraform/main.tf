45ddfa77732d4d82030e8062ec9147e18d34c577
administrateur <administrateur@GERJMCI.m2iformation.local>
2025-09-24 13:07:59 +0200

    Changement : passage de 1 à 2 réplicas

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -36,7 +36,7 @@ resource "kubernetes_deployment_v1" "deploy" {
     labels = { app = var.app }
   }
   spec {
-    replicas = 1
+    replicas = 2
     selector {
       match_labels = { app = var.app }
     }
